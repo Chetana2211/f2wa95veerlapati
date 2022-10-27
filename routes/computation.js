132db92eda13310bf3ec11564c4f9ee762ddbeee
Chetana2211 <108295579+Chetana2211@users.noreply.github.com>
2022-10-26 19:46:45 -0500

    added Bonus computaion

---
+++ routes/computation.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  var v;
+
+  var random=Math.random();
+  console.log(req.query.v);
+  x=req.query.v;
+
+  if(v==undefined){
+    v=random.toFixed(2);
+  }
+
+  let cos=Math.cos(v).toFixed(2)
+  let asin=Math.asin(v).toFixed(2)
+  let asinh=Math.asinh(v).toFixed(2)
+
+  res.render('computaion', { 
+    title: 'Computation',
+  Calculate: 'applied to ' + s+ ' is ' + cos,
+  c1: 'applied to ' + v + ' is ' + asin,
+  c2: 'applied to ' + v + ' is ' + asinh,
+ });
+});
+
+module.exports = router;
